c3aa2cf90faa366b22f0d2ba5e47de3473e481ed
Uoc Azizah <uoctamika.codeberg.org@gmail.com>
2026-06-12 22:15:00 +0000

    changing args.h into argument.h

---
+++ bootstrap/include/tracey/argument/argument.h
@@ -0,0 +1,10 @@
+#ifndef TRACEY_H
+#define TRACEY_H
+
+#include <tracey/struct/args_struct.h>
+
+int parse_args(int argc, char **argv, TraceyOptions *opts);
+void debug_print_args(const TraceyOptions *opts);
+void compile(const TraceyOptions *opts);
+
+#endif
