dcc6dd4e94bf7eee25069713b996748be6d0a72a
Uoc Azizah <uoctamika.codeberg.org@gmail.com>
2026-06-12 22:14:38 +0000

    removing args.h

--- bootstrap/include/tracey/argument/args.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRACEY_H
-#define TRACEY_H
-
-#include <tracey/struct/args_struct.h>
-
-int parse_args(int argc, char **argv, TraceyOptions *opts);
-void compile(const TraceyOptions *opts);
-
-#endif
