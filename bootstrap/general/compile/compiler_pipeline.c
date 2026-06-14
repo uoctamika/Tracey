6ea56da314580ae652c4c1c2b03e1dccf229bdc2
Uoc Azizah <uoctamika.codeberg.org@gmail.com>
2026-06-14 02:52:07 +0000

    updating compile function with open debug close function file

--- bootstrap/general/compile/compiler_pipeline.c
+++ bootstrap/general/compile/compiler_pipeline.c
@@ -1,8 +1,23 @@
+#include <stdio.h>
 #include <tracey/compile/compile.h>
+#include <tracey/file/file.h>
 
 int compile(TraceyOptions *opts)
 {
+    TraceyFile file = {0};
 
+    if (tracey_file_open(&file, opts->input_file) != 0)
+    {
+        fprintf(stderr,
+                "failed to open '%s'\n",
+                opts->input_file);
+
+        return 1;
+    }
+
+   tracey_debug_file_print(&file);
+
+   tracey_file_close(&file);
 
     return 0;
 }
